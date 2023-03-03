2ac10f2b40cf96bf89ec18e0f1aa33be0d3a7ade
BigHeadliner <bigheadliner@gmail.com>
2023-03-03 13:04:16 +0200

    header-slider section ready

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+$(function () {   
+   
+  const hamburger = document.getElementById('menu__btn')
+  const headerMenu = document.getElementById('menu__list')
+  const menuLinks = document.querySelectorAll('.menu__link')
+
+
+  hamburger.addEventListener('click', () => {
+      headerMenu.classList.toggle('menu__list--active')
+      hamburger.classList.toggle('menu__btn--open')
+  })
+
+  menuLinks.forEach(link => {
+      link.addEventListener('click', () => {
+          headerMenu.classList.remove('menu__list--active')
+          hamburger.classList.remove('menu__btn--open')
+      })
+  })  
+   
+    
+    $('.main__slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+      });  
+       
+   $('.menu__item--product').hover( () => { 
+    $('.menu__link-decor').toggleClass('menu__link-decor--rotate');
+   });  
+       
+});
